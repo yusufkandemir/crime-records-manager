5a3f6bb54834f22321e00c2a7d6d7d5763f51cfa
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-12-10 16:21:20 +0300

    feat(server): Added CrimeContext and configuration

---
+++ server/Models/CrimeContext.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrimeRecordsManager.Models
+{
+    public class CrimeContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CrimeContext() : base("name=CrimeContext")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<PoliceStation> PoliceStations { get; set; }
+
+        public DbSet<PoliceOfficer> PoliceOfficers { get; set; }
+    }
+}
