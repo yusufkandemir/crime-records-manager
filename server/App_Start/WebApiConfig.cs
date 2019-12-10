126bcc12594b010dc7875da93d284f6b6e9f48a8
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-12-10 16:24:34 +0300

    server: Configured output formatter

--- server/App_Start/WebApiConfig.cs
+++ server/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace CrimeRecordsManager
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
