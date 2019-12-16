2effc093fb79df70b9cd9a0c6ece595daa81367d
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-12-17 02:30:27 +0300

    Revert "Delete WebApiConfig.cs"
    This reverts commit c0a1cff4f4e43dcaff838677f075c655d4fb6bbf.

---
+++ server/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using CrimeRecordsManager.Models;
+
+namespace CrimeRecordsManager
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<PoliceStation>("PoliceStations");
+            builder.EntitySet<PoliceOfficer>("PoliceOfficers");
+            builder.EntitySet<Suspect>("Suspects");
+            builder.EntitySet<Complaint>("Complaints");
+            builder.EntitySet<Complainant>("Complainants");
+            builder.EntitySet<InvestigationReport>("InvestigationReports");
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: "api",
+                model: builder.GetEdmModel()
+            );
+            config.Count().Filter().OrderBy().Expand().Select().MaxTop(100);
+        }
+    }
+}
